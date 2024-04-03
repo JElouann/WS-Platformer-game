d81aa82aa6bd218e226e6693076f149cb7180277
tymd <lienxay49.jk@gmail.com>
2024-04-03 11:37:02 +0200

    Revert "Merge branch 'main' into Pissith"
    This reverts commit d2d3b4275e98de9200484b7d9ab4b2fb361547e7, reversing
    changes made to 756501bb83f7e7053fed8f13b4a9b1924560de12.

--- WS-Romain-Platformer/Assets/Scripts/Player/PlayerMovement.cs
+++ WS-Romain-Platformer/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,50 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private PlayerInput _input;
-    private Rigidbody2D _rb;
-
-    [SerializeField]
-    private float _moveSpeed;
-    [SerializeField]
-    private float _acceleration;
-    [SerializeField]
-    private float _decceleration;
-    [SerializeField]
-    private float _velPower;
-    private PlayerVFXs _playerVFXs;
-    private float _lastFrameVelocity;
-    private float _currentFrameVelocity;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        _input = GetComponent<PlayerInput>();
-        _rb = GetComponent<Rigidbody2D>();
-        var inputManager = this.GetComponent<InputManager>();
-        _playerVFXs = GetComponent<PlayerVFXs>();
+        
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        var dir = this._input.actions.FindAction("Movement").ReadValue<Vector2>();
-        //this._rb.velocity = new Vector2(dir.x * this._moveSpeed, _rb.velocity.y);
-
-        float targetSpeed = dir.x * _moveSpeed;
-        float speedDif = targetSpeed - _rb.velocity.x;
-        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? _acceleration : _decceleration;
-        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, _velPower) * Mathf.Sign(speedDif);
-        _rb.AddForce(movement * Vector2.right);
-
-        if (_rb.velocity.x == 0)
-        {
-            _playerVFXs.SimpleScreenShake(0.75f, 1);
-        }
+        
     }
 }

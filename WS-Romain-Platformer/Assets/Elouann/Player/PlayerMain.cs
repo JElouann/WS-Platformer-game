c755a02a6a388f63512c75ef33dcaccfb2a68e61
Elouann <elouann.jouenne@e-artsup.net>
2024-04-04 10:39:42 +0200

    Movement Patched

--- WS-Romain-Platformer/Assets/Elouann/Player/PlayerMain.cs
+++ WS-Romain-Platformer/Assets/Elouann/Player/PlayerMain.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class PlayerMain : MonoBehaviour
 {
     public SO_PlayerData _data;
+    private PlayerRoll _roll;
+
+    public bool isRolling;
+
+    public void SwitchRollMode(bool enabled)
+    {
+        if (enabled)
+        {
+            isRolling = true;
+        }
+        else
+        {
+            isRolling = false;
+            _roll.StopRoll();
+        }
+    }
+
+    private void Awake()
+    {
+        _roll = GetComponent<PlayerRoll>();
+    }
 }
